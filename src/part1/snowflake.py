20af08efd8b3faa0a5ebf3d826c393704ff3a492
rdontsov01 <rdontsov01@gmail.com>
2026-09-22 21:51:01 +0300

    fix_logic_errors

--- src/part1/snowflake.py
+++ src/part1/snowflake.py
@@ -7,23 +7,25 @@ from .constants import (
     NODE_ID_BITS,
     NODE_ID_DEFAULT,
     NODE_ID_MAX,
+    NODE_ID_SHIFT,
     SEQUENCE_ID_BITS,
     SEQUENCE_ID_MAX,
     TIMESTAMP_MS_MAX,
+    TIMESTAMP_SHIFT,
 )
 
 
 def read_current_millis(epoch_ms: int) -> int:
-    return int(time.time() * 1000 - epoch_ms)
+    return int(time.time() * 1000) - epoch_ms
 
 
 def decode_timestamp_ms(snowflake_id: int, epoch_ms: int = EPOCH_MS_DEFAULT) -> int:
-    return snowflake_id >> (NODE_ID_BITS + SEQUENCE_ID_BITS) + epoch_ms
+    return (snowflake_id >> (TIMESTAMP_SHIFT)) + epoch_ms
 
 
 def decode_node_id(snowflake_id: int) -> int:
     mask = (1 << NODE_ID_BITS) - 1
-    return (snowflake_id >> SEQUENCE_ID_BITS) & mask
+    return (snowflake_id >> NODE_ID_SHIFT) & mask
 
 
 def decode_sequence_id(snowflake_id: int) -> int:
@@ -36,21 +38,22 @@ def generate_snowflake_id(
     node_id: int = NODE_ID_DEFAULT,
     epoch_ms: int = EPOCH_MS_DEFAULT,
 ) -> int | None:
-    if not (0 < node_id < NODE_ID_MAX):
-        print("node_id must be in [0, " + str(NODE_ID_MAX) + "], got " + str(node_id))
-    elif not (0 < sequence_id < SEQUENCE_ID_MAX):
-        print(
-            "sequence_id must be in [0, "
-            + str(SEQUENCE_ID_MAX)
-            + "], got "
-            + str(sequence_id)
-        )
-    elif read_current_millis(epoch_ms) > TIMESTAMP_MS_MAX:
+
+    if not (0 <= node_id <= NODE_ID_MAX):
+        print(f"node_id must be in [0, {NODE_ID_MAX}], got {node_id}")
+        return None
+
+    if not (0 <= sequence_id <= SEQUENCE_ID_MAX):
+        print(f"sequence_id must be in [0, {SEQUENCE_ID_MAX}], got {sequence_id}")
+        return None
+
+    current_millis = read_current_millis(epoch_ms)
+
+    if not (0 <= current_millis <= TIMESTAMP_MS_MAX):
         print("overflows")
-    else:
-        return (
-            (read_current_millis(epoch_ms) << (NODE_ID_BITS + SEQUENCE_ID_BITS))
-            | (node_id << NODE_ID_BITS)
-            | (sequence_id)
-        )
-    return None
+        return None
+    return (
+        (current_millis << (TIMESTAMP_SHIFT))
+        | (node_id << NODE_ID_SHIFT)
+        | (sequence_id)
+    )
